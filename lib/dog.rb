1a8bfafd5ffae6651a90c548335e9fd3195269da
Rocky Chiang <rocky@chem.ucla.edu>
2018-03-06 21:02:09 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,14 @@
 class Dog
-  def name=(new_name)
-    @name = new_name
+  def name=(name)
+    @name = name
   end
 
   def name
     @name
   end
 
-  def breed=(new_breed)
-    @breed = new_breed
+  def breed=(breed)
+    @breed = breed
   end
 
   def breed
